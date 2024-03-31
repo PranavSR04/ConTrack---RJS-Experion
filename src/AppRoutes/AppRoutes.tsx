15ee1b08940338d7358c5ed447e27863ee062225
Pranav S R <psrdev04@gmail.com>
2024-03-31 12:55:30 +0530

    Fix:Folder structure

--- src/AppRoutes/AppRoutes.tsx
+++ src/AppRoutes/AppRoutes.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBarHandler from "../Components/NavBar/NavBarHandler";
+import LoginHandler from "../Features/Login/LoginHandler";
+import AuthContext from "../Components/AuthContext/AuthContext";
 
 const AppRoutes = () => {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<></>}></Route>
-        </Routes>
-    </BrowserRouter>
-  )
-}
+	return (
+		<BrowserRouter>
+			<AuthContext>
+				<Routes>
+					<Route path="/" element={<LoginHandler />}></Route>
+					<Route path="/navbar" element={<NavBarHandler />}></Route>
+				</Routes>
+			</AuthContext>
+		</BrowserRouter>
+	);
+};
 
-export default AppRoutes
+export default AppRoutes;
