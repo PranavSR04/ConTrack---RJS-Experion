1bb2c844886fad8c08908609ceea38cc3740a6a5
Dantus George Tom <dantus.tom@experionglobal.local>
2024-04-02 21:00:41 +0530

    to new repo

--- src/AppRoutes/AppRoutes.tsx
+++ src/AppRoutes/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import ManageUsersHandler from '../Features/ManageUsers/ManageUsersHandler'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<></>}></Route>
+        <Route path="/ManageUser" element={<><ManageUsersHandler/></>}></Route>
         </Routes>
     </BrowserRouter>
   )
