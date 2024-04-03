fb415081fc5b20f2ccee32381ddf2d1fccf46505
Boby <boby.benny@experionglobal.com>
2024-04-03 21:09:17 +0530

    feat:toast component

--- src/Features/Dashboard/Dashboard.tsx
+++ src/Features/Dashboard/Dashboard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Card } from "antd";
 import styles from "./dashboard.module.css";
 import DashBoardNotificationListHandler from "../../Components/DashBoardNotificationList/DashBoardNotificationListHandler";
