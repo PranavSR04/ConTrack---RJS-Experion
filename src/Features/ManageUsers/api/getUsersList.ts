eb5773dd3913dc85cf7fe18c6a01a0db6ffda0ed
Dantus George Tom <dantus.tom@experionglobal.local>
2024-04-22 15:47:32 +0530

    feat:Manage Users - Grouping System

--- src/Features/ManageUsers/api/getUsersList.ts
+++ src/Features/ManageUsers/api/getUsersList.ts
@@ -1,4 +1,6 @@
 import axiosInstance from "../../../Config/AxiosConfig";
-export const getUsersList = async () => {
-  return await axiosInstance.get("api/users/list");
+export const getUsersList = async (search:string) => {
+  return await axiosInstance.get(`/api/users/list?${search ? `search=${search}`: "" }`);
 };
+
+// get(`/api/experion/list?${searchValue ? `name=${searchValue}` : ""}`);
