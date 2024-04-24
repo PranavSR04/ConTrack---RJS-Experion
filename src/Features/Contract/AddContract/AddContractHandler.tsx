6709dcab698f9a3d58213b793237e651bb5f8a84
Pranav S R <psrdev04@gmail.com>
2024-04-24 14:18:24 +0530

    \Feat: Add and Edit Contract Created

---
+++ src/Features/Contract/AddContract/AddContractHandler.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AddContract from './AddContract'
+
+const AddContractHandler = () => {
+
+  return (
+    <AddContract/>
+  )
+}
+
+export default AddContractHandler
---
+++ src/Features/Contract/EditContract/EditContract.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditContract = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditContract
