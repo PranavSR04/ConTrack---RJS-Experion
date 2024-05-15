0e1ff8cd097eebbe3179e9e1463cd829dfb2979d
Pranav S R <psrdev04@gmail.com>
2024-05-15 15:12:11 +0530

    Feat: Added Unauthorized page

--- src/Components/NavBar/NavBarHandler.tsx
+++ src/Components/NavBar/NavBarHandler.tsx
@@ -2,12 +2,16 @@ import React, { useContext, useEffect, useState } from 'react'
 import NavBar from './NavBar'
 import { Auth } from '../AuthContext/AuthContext'
 import { NavContexts } from '../NavContext/NavContext';
+import { useNavigate } from 'react-router';
 
 const NavBarHandler = () => {
   const {currentUser} = useContext(Auth);
   const{activeNotificationCount}=useContext(NavContexts);
-
+  const navigate = useNavigate();
   console.log(currentUser);
+  if(!currentUser){
+		navigate("/unauthorized");
+	}
 
   const [username,setUsername] = useState<string>();
   const [notificationCount,setNotificationCount] = useState();
@@ -18,13 +22,12 @@ const NavBarHandler = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[]);
 
-  return(
+  return username?(
     <NavBar
     username={username}
     activeNotificationCounte={activeNotificationCount}
-    
     />
-  )
+  ):null
 }
 
 export default NavBarHandler
