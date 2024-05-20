3b67aac9b43a5e527901aab04e63a58341efbb3d
Pranav S R <psrdev04@gmail.com>
2024-05-20 12:07:07 +0530

    Fix: Dashboard

--- src/Components/DoughnutChart/DoughnutChart.tsx
+++ src/Components/DoughnutChart/DoughnutChart.tsx
@@ -1,17 +1,37 @@
-import { DougnutChartPropsType, apiData } from './types';
-import { Doughnut } from 'react-chartjs-2';
-import { Card } from 'antd';
+import { DougnutChartPropsType, apiData } from "./types";
+import { Doughnut } from "react-chartjs-2";
+import { Card } from "antd";
 
-const DoughnutChart = ({loading, chartData, options}:DougnutChartPropsType) => {    
-  return (
-    <>
-    <div style={{ width:'16vw'}}>
-   <Card style={{ height: '35vh '}}> {loading?<div>Loading...</div>:<><Doughnut data={chartData} options={options}></Doughnut>
-    <p style={{fontSize:'.7rem',textAlign:'center',fontWeight:'600'}}>Contract Status</p></>}
-  </Card>
-      </div>
-    </>
-  )
-}
+const DoughnutChart = ({
+	loading,
+	chartData,
+	options,
+}: DougnutChartPropsType) => {
+	return (
+		<>
+			{/* <div style={{ width:'16vw'}}> */}
+			<Card style={{height:"37.3vh",width:"285px"}} bodyStyle={{padding:0,paddingTop:"3vh"}}>
+				{" "}
+				{loading ? (
+					<div>Loading...</div>
+				) : (
+					<>
+						<Doughnut data={chartData} options={options} style={{height:1}}></Doughnut>
+						<p
+							style={{
+								fontSize: ".7rem",
+								textAlign: "center",
+								fontWeight: "600",
+							}}
+						>
+							Contract Status
+						</p>
+					</>
+				)}
+			</Card>
+			{/* </div> */}
+		</>
+	);
+};
 
-export default DoughnutChart
+export default DoughnutChart;
