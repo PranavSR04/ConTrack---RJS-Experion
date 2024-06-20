d96a09eabe69ed2ad1e087c1b24639d613c59b28
Gokul Surendran <gokul.surendran@experionglobal.local>
2024-06-20 11:44:22 +0530

    dashboard

--- src/Features/Dashboard/Dashboard.tsx
+++ src/Features/Dashboard/Dashboard.tsx
@@ -12,6 +12,7 @@ import DoughnutChartHandler from "../../Components/DoughnutChart/DoughnutChartHa
 import ScatterPlotHandler from "../../Components/RevenueScatterPlot/ScatterPlotHandler";
 import RegionHorizontalBar from "../../Components/RegionHorizontalBar/RegionHorizontalBar";
 import TopRevenueHandler from "../../Components/TopRevenueRegion/TopRevenueHandler";
+import Header from './../Contract/IndividualContract/Header/Header';
 
 const Dashboard = () => {
 	return (
@@ -19,18 +20,18 @@ const Dashboard = () => {
 			<Row style={{paddingBottom:8}}>
 				<Col className={styles.col} span={17}>
 					<Row gutter={140} style={{paddingBottom:8}}>
-						<Col span={4}><DashBoardMonthlyRevenueHandler /></Col>
-						<Col span={4}><DashBoardQuaterlyRevenueHandler /></Col>
-						<Col span={4}><DashBoardYearlyRevenueHandler /></Col>
-						<Col span={4}><DashBoardContractCountHandler /></Col>
-						<Col span={4}><DashBoardMsaCountHandler /></Col>
+						<Col span={4} className={styles.headercol1}><DashBoardMonthlyRevenueHandler /></Col>
+						<Col span={4} className={styles.headercol2}><DashBoardQuaterlyRevenueHandler /></Col>
+						<Col span={4} className={styles.headercol3}><DashBoardYearlyRevenueHandler /></Col>
+						<Col span={4} className={styles.headercol4}><DashBoardContractCountHandler /></Col>
+						<Col span={4} className={styles.headercol5}><DashBoardMsaCountHandler /></Col>
 					</Row>
 					<Row gutter={15}>
 						<Col span={14}><Card className={styles.barchart_card} bodyStyle={{paddingTop:"5vh"}}><BarChartHandler /></Card></Col>
 						<Col span={8}><DoughnutChartHandler /></Col>
 					</Row>
         		</Col>
-				<Col className={styles.col} span={5} ><DashBoardNotificationListHandler /></Col>
+				<Col className={styles.col1} span={5} ><DashBoardNotificationListHandler /></Col>
 			</Row>
 			<Row gutter={15}>
 				<Col span={8}><Card className={styles.lastrow_card}><ScatterPlotHandler /></Card></Col>
